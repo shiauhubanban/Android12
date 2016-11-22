92fa9a56f274ccdf23802615533646ed52ba444b
user <a>
2016-11-22 10:35:54 +0800

    v2

--- app/src/main/java/com/org/iii/shine12/MainActivity.java
+++ app/src/main/java/com/org/iii/shine12/MainActivity.java
@@ -2,6 +2,12 @@ package com.org.iii.shine12;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    // UDP Sender
+    public void test1(View v){
+        byte[] buf = "Hello, Brad".getBytes();
+        try {
+            DatagramSocket socket = new DatagramSocket();
+            DatagramPacket packet = new DatagramPacket(buf, buf.length,
+                    InetAddress.getByName("10.0.3.2"), 8888);
+            socket.send(packet);
+            socket.close();
+            Log.v("brad", "UDP Send OK");
+        }catch (Exception e){
+            Log.v("brad", e.toString());
+        }
     }
 }
