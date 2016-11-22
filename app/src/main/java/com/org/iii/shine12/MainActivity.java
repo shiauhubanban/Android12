96c616ab195b410d4f9709eba3a8ed0162869434
user <a>
2016-11-22 12:12:54 +0800

    v5

--- app/src/main/java/com/org/iii/shine12/MainActivity.java
+++ app/src/main/java/com/org/iii/shine12/MainActivity.java
@@ -1,22 +1,35 @@
 package com.org.iii.shine12;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.HttpURLConnection;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private UIHandler uiHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        uiHandler = new UIHandler();
+        textView = (TextView)findViewById(R.id.tv);
     }
 
     // UDP Sender
@@ -56,6 +69,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test3(View v){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://www.iii.org.tw/");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.connect();
+                    BufferedReader reader =
+                            new BufferedReader(
+                                    new InputStreamReader(conn.getInputStream()));
+                    String line; StringBuilder sb = new StringBuilder();
+                    while ( (line = reader.readLine()) != null){
+                        sb.append(line + "\n");
+                    }
+                    reader.close();
 
+                    Message mesg = new Message();
+                    Bundle data = new Bundle();
+                    data.putCharSequence("data", sb);
+                    mesg.setData(data);
+                    uiHandler.sendMessage(mesg);
+
+
+                } catch (Exception e) {
+                    Log.v("brad", e.toString());
+                }
+
+            }
+        }.start();
     }
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+
+        }
+    }
+
+
 }
